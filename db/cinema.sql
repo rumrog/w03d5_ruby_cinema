efb8a7cae8811124d2e6554b98a3767ff2ff1194
Rumen Roger <rumenroger@gmail.com>
2020-05-29 22:06:59 +0100

    cinema.sql setup

--- db/cinema.sql
+++ db/cinema.sql
@@ -2,5 +2,21 @@ DROP TABLE IF EXISTS tickets;
 DROP TABLE IF EXISTS customers;
 DROP TABLE IF EXISTS movies;
 
-CREATE TABLE movies;
-  id SERIAL PRIMARY KEY
+CREATE TABLE movies (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  price INT
+);
+
+CREATE TABLE customers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT
+);
+
+CREATE TABLE tickets (
+  id SERIAL PRIMARY KEY,
+  customer_id INT REFERENCES customers(id) ON DELETE CASCADE,
+  movie_id INT REFERENCES movies(id) ON DELETE CASCADE
+);
+
