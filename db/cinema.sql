babd0dc8f5f6a735079f9f1cc2b6c7b58fe5b5ca
Rumen Roger <rumenroger@gmail.com>
2020-05-29 22:20:51 +0100

    cinema.sql populated

--- db/cinema.sql
+++ db/cinema.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS tickets;
 DROP TABLE IF EXISTS customers;
-DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS films;
 
-CREATE TABLE movies (
+CREATE TABLE films (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255),
   price INT
@@ -16,7 +16,18 @@ CREATE TABLE customers (
 
 CREATE TABLE tickets (
   id SERIAL PRIMARY KEY,
-  customer_id INT REFERENCES customers(id) ON DELETE CASCADE,
-  movie_id INT REFERENCES movies(id) ON DELETE CASCADE
+  film_id INT REFERENCES films(id) ON DELETE CASCADE,
+  customer_id INT REFERENCES customers(id) ON DELETE CASCADE
 );
 
+INSERT INTO customers (name) VALUES ('Sir Cody MacDuck');
+INSERT INTO customers (name) VALUES ('Vincent Freeman');
+INSERT INTO customers (name) VALUES ('Duncan MacLeod');
+
+INSERT INTO films (title, price) VALUES ('CodeClan: The film', 10);
+INSERT INTO films (title, price) VALUES ('Gataca', 10);
+INSERT INTO films (title, price) VALUES ('Highlander', 10);
+
+INSERT INTO tickets (film_id, customer_id) VALUES (1, 1);
+INSERT INTO tickets (film_id, customer_id) VALUES (2, 2);
+INSERT INTO tickets (film_id, customer_id) VALUES (3, 3);
